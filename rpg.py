38370be2cf8754d66cb3f0c2c5956ea3831d3f85
Justin Boyd <jrboyd90@gmail.com>
2017-09-19 13:12:48 -0500

    Step 1

---
+++ rpg.py
@@ -0,0 +1,12 @@
+#Making a Hero class
+class Hero:
+    def __init__(self,health='10',power='5'):
+        self.health = health
+        self.power = power
+    def attack(enemy):
+
+# Making a Goblin class
+class Goblin:
+    def __init__(self,health='6',power='2'):
+        self.health = health
+        self.power = power
