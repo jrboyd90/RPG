b3a007cd548d3dca62b81c55125689f517791dde
Justin Boyd <jrboyd90@gmail.com>
2017-09-19 13:25:56 -0500

    step 1

---
+++ rpgClass.py
@@ -0,0 +1,16 @@
+#Making a Hero class
+class Hero:
+    def __init__(self,health='10',power='5'):
+        self.health = health
+        self.power = power
+    def attack(enemy):
+        goblin_health -= hero_power
+            print("You do {} damage to the goblin.".format(hero_power))
+            if goblin_health <= 0:
+                print("The goblin is dead.")
+
+# Making a Goblin class
+class Goblin:
+    def __init__(self,health='6',power='2'):
+        self.health = health
+        self.power = power
