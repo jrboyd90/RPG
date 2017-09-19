dc62c01a7b0fb3575b00df808f0b5cb327b76fa2
Justin Boyd <jrboyd90@gmail.com>
2017-09-19 14:28:13 -0500

    step 6

--- rpgClass.py
+++ rpgClass.py
@@ -1,9 +1,9 @@
-
-#Making a Hero class
-class Hero:
-    def __init__(self,health='10',power='5'):
+class Character:
+    def__init__(self,health,power):
         self.health = health
         self.power = power
+#Making a Hero class
+class Hero (Character):
     def attack(enemy):
         enemy.health -= self.power
             print("You do {} damage to the goblin.".format(self.power))
@@ -18,10 +18,7 @@ class Hero:
         print("You have {} health and {} power.".format(self.health, self.power))
 
 # Making a Goblin class
-class Goblin:
-    def __init__(self,health='6',power='2'):
-        self.health = health
-        self.power = power
+class Goblin (Character):
     def attack(enemy):
         enemy.health -= self.power
             print("The goblin does {} damage to you.".format(self.power))
