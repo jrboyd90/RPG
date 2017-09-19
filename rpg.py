a2a2a24afe843d052e900d925c93cd197a8c03d6
Justin Boyd <jrboyd90@gmail.com>
2017-09-19 13:26:39 -0500

    step 2

--- rpg.py
+++ rpg.py
@@ -1,12 +1,32 @@
-#Making a Hero class
-class Hero:
-    def __init__(self,health='10',power='5'):
-        self.health = health
-        self.power = power
-    def attack(enemy):
+def main():
+    link = Hero()
+    goblin = Goblin()
 
-# Making a Goblin class
-class Goblin:
-    def __init__(self,health='6',power='2'):
-        self.health = health
-        self.power = power
+    while goblin.health > 0 and link.health > 0:
+        print("You have {} health and {} power.".format(link.health, link.power))
+        print("The goblin has {} health and {} power.".format(goblin.health, goblin.power))
+        print()
+        print("What do you want to do?")
+        print("1. fight goblin")
+        print("2. do nothing")
+        print("3. flee")
+        print("> ", end=' ')
+        raw_input = input()
+        if raw_input == "1":
+            link.attack(goblin)
+        elif raw_input == "2":
+            pass
+        elif raw_input == "3":
+            print("Goodbye.")
+            break
+        else:
+            print("Invalid input {}".format(raw_input))
+
+        if goblin_health > 0:
+            # Goblin attacks hero
+            hero_health -= goblin_power
+            print("The goblin does {} damage to you.".format(goblin_power))
+            if hero_health <= 0:
+                print("You are dead.")
+
+main()
